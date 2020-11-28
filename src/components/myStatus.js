6f2762b9eacbb6e6f2a947ef9ed20d8e4772ff33
Onishi Takeshi <march.hare.come@gmail.com>
2020-11-29 02:09:03 +0900

    status管理の構想だけコメント

--- src/components/myStatus.js
+++ src/components/myStatus.js
@@ -13,3 +13,15 @@ const MyStatus = () => {
 }
 
 export default MyStatus
+
+// ステータス構想
+// firebaseに↓  のようなチャンネル作る
+// userStatus だけ管理 (state: userStatusList
+//     - セットしたりするのに楽だから layout ContextにもmyUserStatusId, myUserStatusLabelも用意
+//        - watchはUserIconで行う
+//     - { 'gi3kg8': { status: '話しかけるなID', label: 'ラベル' } }
+//     - status, 1: 無理, 2: OK, 3: むしろ話しかけてくれ, それ以外は退出
+//     - labelはフリー入力させる (プルダウンで更に選ばせる)
+//     - というか、 ステータス選択と横にラベル入力でいい (ラベル入力はプルダウンのデフォ値使う)
+// userIcon だけ管理 (state: userIconList
+//     - これはGravatorの次の作業時
