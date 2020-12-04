375f3e1408120a17b01a33e837b613d13a346500
sp_DEGU <degudegu2510@gmail.com>
2020-12-04 23:49:57 +0900

    fixed logo style

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,16 +1,22 @@
 import React from "react"
-import IconSquare from "../images/icon_small.jpg"
+import IconSquare from "../images/logo_horizontal.png"
 
 const Logo = () => {
   return(
     <div className='logo' style={{
+      backgroundColor: '#FFF',
+      display: 'inline-block',
+      padding: '16px 24px',
       position: 'absolute',
-      width: '50px',
-      height: '50px',
-      top: '50px',
-      left: '50px',
+      top: '32px',
+      left: '32px',
+      borderRadius: '4px',
+      boxShadow: '0px 1px 4px rgba(0,0,0,0.24)'
     }}>
-    <img src={IconSquare} style={{width: '100%'} }/>
+    <img src={IconSquare} style={{
+      width: '200px',
+      verticalAlign: 'top',
+      } }/>
     </div>
   )
 }
